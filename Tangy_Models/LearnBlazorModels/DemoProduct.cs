bb818a6aecb19282b484a2efe694dcec2dc27f81
Rory <games@rory.es>
2022-07-13 20:17:44 +0200

    Model added

---
+++ Tangy_Models/LearnBlazorModels/DemoProduct.cs
@@ -0,0 +1,10 @@
+﻿namespace Tangy_Models.LearnBlazorModels;
+
+public class DemoProduct
+{
+	public int Id { get; set; }
+	public string Name { get; set; }
+	
+	public double Price { get; set; }
+	public bool IsActive { get; set; }
+}
