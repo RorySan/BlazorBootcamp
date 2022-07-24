a2efd38d31251809d5eb518562aa3fed5a3c1fa7
Rory <games@rory.es>
2022-07-24 11:32:30 +0200

    category list

--- Tangy_Models/CategoryDTO.cs
+++ Tangy_Models/CategoryDTO.cs
@@ -1,7 +1,10 @@
-﻿namespace Tangy_Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tangy_Models;
 
 public class CategoryDTO
 {
 	public int Id { get; set; }
+	[Required(ErrorMessage = "Please enter name")]
 	public string Name { get; set; }
 }
