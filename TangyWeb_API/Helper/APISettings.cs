bd8f04d2b45084d6fd98d1958dcaa092b90af44b
Rory <games@rory.es>
2022-08-05 10:40:33 +0200

    api authentication implemented

---
+++ TangyWeb_API/Helper/APISettings.cs
@@ -0,0 +1,10 @@
+﻿namespace TangyWeb_API.Helper;
+/// <summary>
+/// This class has the exact names of the configuration we want to access in appsettings.json
+/// </summary>
+public class APISettings
+{
+	public string SecretKey { get; set; }
+	public string ValidAudience { get; set; }
+	public string ValidIssuer { get; set; }
+}
--- TangyWeb_API/Program.cs
+++ TangyWeb_API/Program.cs
@@ -1,9 +1,14 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Tangy_Business.Repository;
 using Tangy_Business.Repository.IRepository;
 using Tangy_DataAccess;
 using Tangy_DataAccess.Data;
+using TangyWeb_API.Helper;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,13 +17,65 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+	options.SwaggerDoc("v1", new OpenApiInfo { Title = "TangWeb_Api", Version = "v1" });
+	options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+	{
+		In = ParameterLocation.Header,
+		Description = "Please Bearer and then token in the field",
+		Name = "Authorization",
+		Type = SecuritySchemeType.ApiKey
+	});
+	options.AddSecurityRequirement(new OpenApiSecurityRequirement {
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference
+				{
+					Type = ReferenceType.SecurityScheme,
+					Id = "Bearer"
+				}
+			},
+			new string[] { }
+		}
+	});
+});
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddDefaultTokenProviders()
 	.AddEntityFrameworkStores<ApplicationDbContext>();
+
+var apiSettingsSection = builder.Configuration.GetSection("APISettings");
+builder.Services.Configure<APISettings>(apiSettingsSection); // this will automatically assign all values from APISettings section in appsettings.json to the variables inside APISettings object.
+
+var apiSettings = apiSettingsSection.Get<APISettings>();
+var key = Encoding.ASCII.GetBytes(apiSettings.SecretKey);
+builder.Services.AddAuthentication(options =>
+{
+	options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+	options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+	options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+	options.RequireHttpsMetadata = false;
+	options.SaveToken = true;
+	options.TokenValidationParameters = new()
+	{
+		ValidateIssuerSigningKey = true,
+		IssuerSigningKey = new SymmetricSecurityKey(key),
+		ValidateAudience = true,
+		ValidateIssuer = true,
+		ValidAudience = apiSettings.ValidAudience,
+		ValidIssuer = apiSettings.ValidIssuer,
+		ClockSkew = TimeSpan.Zero
+	};
+});
+
 // instead of allowing all, it can be narrowed down to specific origins
 builder.Services.AddCors(options => options.AddPolicy("Tangy", policyBuilder =>
 {
@@ -38,7 +95,7 @@ app.UseHttpsRedirection();
 // here we use the same name that we defined in the AddPolicy method above
 app.UseCors("Tangy");
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
